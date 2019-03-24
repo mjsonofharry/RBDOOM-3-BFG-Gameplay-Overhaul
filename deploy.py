280e9115ec6fb5a11e4b5cb3db53030832ff395a
mjharrison <mjsonofharry@gmail.com>
2019-03-24 01:44:20 -0400

    autoinstall; could speed up with multithreading

--- deploy.py
+++ deploy.py
@@ -1,9 +1,10 @@
 import collections
 import os
+import shutil
 import sys
 import zipfile
 
-Mod = collections.namedtuple('Mod', ['name', 'is_pk4', 'data_sources'])
+Mod = collections.namedtuple('Mod', ['name', 'is_pk4', 'data_sources', 'install_path'])
 
 EXCLUSIONS = [
     '.zip',
@@ -18,9 +19,9 @@ EXCLUSIONS = [
 ]
 
 MOD_DEFINITIONS = [
-    Mod('RBDOOM-3-BFG', is_pk4=False, data_sources=['shared', 'bfg_common', 'bfg_rbdoom']),
-    Mod('Classic-RBDOOM-3-BFG', is_pk4=False, data_sources=['shared', 'bfg_common', 'bfg_classic-rbdoom']),
-    Mod('Doom-3', is_pk4=True, data_sources=['shared', 'd3_common'])
+    Mod('RBDOOM-3-BFG', is_pk4=False, data_sources=['shared', 'bfg_common', 'bfg_rbdoom'], install_path='D:\\RBDOOM-3-BFG'),
+    Mod('Classic-RBDOOM-3-BFG', is_pk4=False, data_sources=['shared', 'bfg_common', 'bfg_classic-rbdoom'], install_path='D:\\Classic-RBDOOM-3-BFG'),
+    Mod('Doom-3', is_pk4=True, data_sources=['shared', 'd3_common'], install_path=None)
 ]
 
 def traverse(root):
@@ -37,11 +38,12 @@ def traverse(root):
 def main():
     project_path = sys.path[0]
     print('Project root:', project_path)
-    for game_name, is_pk4, mod_data_sources in MOD_DEFINITIONS:
+    for game_name, is_pk4, mod_data_sources, install_path in MOD_DEFINITIONS:
         print('Working on:', game_name)
         print('Using sources:', mod_data_sources)
         mod_name = game_name + '-Gameplay-Overhaul'
         target = os.path.join(project_path, mod_name + '.pk4') if is_pk4 else os.path.join(project_path, mod_name + '.zip')
+        os.remove(target)
         with zipfile.ZipFile(target, 'w') as zip:
             print('Generating archive...')
             for data_source, data_paths in [
@@ -51,8 +53,13 @@ def main():
                 data_source_path = os.path.join(project_path, data_source)
                 for path in data_paths:
                     print('Zipping:', path)
-                    arcname = os.path.relpath(path, data_source) if is_pk4 else os.path.join(mod_name, os.path.relpath(path, data_source))
-                    zip.write(path, arcname=arcname)
+                    relative_path = os.path.relpath(path, data_source) if is_pk4 else os.path.join(mod_name, os.path.relpath(path, data_source))
+                    zip.write(path, arcname=relative_path)
+                    if install_path and not is_pk4:
+                        copy_path = os.path.join(install_path, relative_path)
+                        if not os.path.exists(os.path.dirname(copy_path)):
+                            os.makedirs(copy_path)
+                        shutil.copy(path, copy_path)
             print('Wrote:', target)
         print('')
 
