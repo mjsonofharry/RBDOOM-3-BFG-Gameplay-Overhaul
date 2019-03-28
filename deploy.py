c44d82ea6b6fcd9e18918cc48445f54388e42047
mjharrison <mjsonofharry@gmail.com>
2019-03-27 20:49:34 -0400

    better pk4 copy

--- deploy.py
+++ deploy.py
@@ -35,9 +35,12 @@ def create_mod_package(project_path, mod, source_data, install_pk4=False):
     print('Zipped', n, 'files')
     print('Finished packaging for', mod.game_name)
     if install_pk4:
-        secondary_target = os.path.join(mod.game_path, mod_name)
+        secondary_target_dir = os.path.join(mod.game_path, mod_name)
+        if not os.path.isdir(secondary_target_dir):
+            os.mkdir(secondary_target_dir)
+        secondary_target = os.path.join(secondary_target_dir, mod_name + '.pk4')
         shutil.copy(target, secondary_target)
-        print('Copied package to', secondary_target)
+        print('Installed package to', secondary_target)
     print('')
 
 def main():
